16990ca62e18e983a5aeeda30052609bafefc3f8
Nicholas Price <nicholasp@profusion.com>
2023-08-16 14:17:15 +0100

    Update

--- .history/src/app/page_20230816141648.js
+++ .history/src/app/page_20230816141648.js
@@ -53,7 +53,51 @@ export default function Home() {
                     <div className="callback-box bg-light p-4">
                       <h3 className="mb-3">Request a Callback</h3>
                       <div>
-                       
+                        <form
+                          name="dw-callback"
+                          method="POST"
+                          netlify="true"
+                          netlify-honeypot="bot-field"
+                        >
+                          <input
+                            type="hidden"
+                            name="form-name"
+                            value="dw-callback"
+                          />
+                          <label htmlFor="fullname" className="form-label mb-3">
+                            Name:
+                          </label>
+                          <input
+                            type="text"
+                            className="form-control mb-3"
+                            id="fullname"
+                            name="fullname"
+                            placeholder="Name"
+                            required
+                          ></input>
+
+                          <label
+                            htmlFor="phonenumber"
+                            className="form-label mb-3"
+                          >
+                            Phone Number:
+                          </label>
+                          <input
+                            type="tel"
+                            className="form-control mb-3"
+                            id="phonenumber"
+                            name="phonenumber"
+                            placeholder="Phone Number"
+                            required
+                          ></input>
+
+                          <button
+                            type="submit"
+                            className="btn btn-primary btn-submit"
+                          >
+                            Submit
+                          </button>
+                        </form>
                       </div>
                     </div>
                   </div>
